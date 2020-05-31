391bb7fd73a63be33f30217d755ed5549420d6e2
Carlos Gutierrez <ingecarlos.gutierrez@gmail.com>
2020-05-31 15:40:07 -0500

    Adding changes in the file

--- generadorPassword.py
+++ generadorPassword.py
@@ -101,22 +101,55 @@ def randomListArray(n):
       return arrayList
 
 def selectionCharacter(A):
-  for i in range (len(A)):
-    numTemp = A[i]
-    A[i] = CHARACTERS[numTemp]
+  for i in range (0,len(A)):
+    try:
+      numTemp = A[i]
+      A[i] = CHARACTERS[numTemp]
+    except:
+      numTemp = numTemp -1
+      A[i] = CHARACTERS[numTemp]
   return A
 
 def run():
-  l = int(input("Longitud de tu contraseña: "))
-  numberArray = randomListArray(l)
-  numbers = selectionCharacter(numberArray)
-  passwordGenerated = ""
-  for i in range (len(numbers)):
+  valor = False
+  valor2 = False
+  while (valor2 != True):
+    while (valor != True):
+      l = int(input("Longitud de tu contraseña: "))
+      if (l < 6):
+        print("------Error----")
+        print("Elige un numero entre 6 y 50")
+      elif (l > 50):
+        print("------Error----")
+        print("Elige un numero entre 6 y 50")
+      else:
+        valor = True
+    numberArray = randomListArray(l)
+    numbers = selectionCharacter(numberArray)
+    passwordGenerated = ""
+    for i in range (len(numbers)):
       temp = numbers[i]
       passwordGenerated = passwordGenerated + temp
-  print("---------------------o---------------------")
-  print("Contraseña generada: " + passwordGenerated)
-  print("---------------------o---------------------")
+    print("---------------------o---------------------")
+    print("Contraseña generada: " + passwordGenerated)
+    print("---------------------o---------------------")
+    answer = False
+    while (answer != True):
+      try:
+        l2 = int(input("Quieres generar otra contraseña? (1 = YES, 0 = NO): "))
+        if (l2 == 1):
+          valor2 = False
+          answer = True
+          valor = False
+        elif (l2 == 0):
+          valor2 = True
+          answer = True
+        else:
+          print("Elige entre 1 y 0---(1 = YES, 0 = NO)")
+      except:
+        print("Solo puedes seleccionar numeros")
+        answer = False
+
   input('Press ENTER to exit')
 
 if __name__ == '__main__':
